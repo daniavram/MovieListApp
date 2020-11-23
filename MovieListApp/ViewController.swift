d22b524eb46f93d323f0c633d87ad078440706cd
Daniel Avram <daniel.avram@softvision.ro>
2020-11-23 18:26:21 +0200

    Initial Commit

---
+++ MovieListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListApp
+//
+//  Created by daniel.avram on 23/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
